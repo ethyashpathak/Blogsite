a71f927b10853506d351143b63a4585b3f386a84
ethyashpathak <ethyashpathak@gmail.com>
2025-07-03 13:57:32 +0530

    day 1

--- blogsite/src/App.jsx
+++ blogsite/src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    console.log(import.meta.env.VITE_APP_APPWRITE_URL)
   return (
     <>
-     
+     <h1>Hello my love</h1>
     </>
   )
 }
