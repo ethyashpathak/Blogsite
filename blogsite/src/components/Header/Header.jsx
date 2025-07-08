1ba0ff44b7219691b553833d73afe4941110f30f
ethyashpathak <ethyashpathak@gmail.com>
2025-07-08 20:44:23 +0530

    initiated components

---
+++ blogsite/src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import React, { use } from 'react'
+import {Container,Logo,LogoutBtn} from '../index'
+import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+function Header() {
+    const authStatus=useSelector((state)=>state.auth.status)
+    const navigate=useNavigate()
+
+    const naItems=[
+        {
+            name:'Home',
+            slug:"/",
+            active:true
+        },
+        {
+            name:"Login",
+            slug:"/login",
+            active:!authStatus,
+        },
+        {
+            name:"Signup",
+            slug:"/signup",
+            active:!authStatus
+        },
+        {
+            name:"All Posts",
+            slug:"/all-posts",
+            active:authStatus,
+        },
+        {
+            name:"Add Posts",
+            slug:"/add-post",
+            active:authStatus,
+        },
+    ]
+
+    return (
+        <div>Header</div>
+    )
+}
+
+export default Header
