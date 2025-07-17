9d6b11f7b27ee89b17b9f9050a43c553aad94e0e
ethyashpathak <ethyashpathak@gmail.com>
2025-07-17 11:55:41 +0530

    added routes

--- blogsite/src/main.jsx
+++ blogsite/src/main.jsx
@@ -4,12 +4,80 @@ import './index.css'
 import App from './App.jsx'
 import {Provider} from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AuthLayout,Login } from './components/index.js'
+
+import Addpost from './pages/Addpost.jsx'
+import EditPost from './pages/EditPost.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import Home from './pages/Home.jsx'
+import Post from './pages/Posts.jsx'
+import Signup from './pages/Signup.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Login />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Signup />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPosts />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <Addpost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-       <App />
+    <RouterProvider router={router}/>
     </Provider>
-    
   </StrictMode>,
 )
