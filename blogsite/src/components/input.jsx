2bd830377bd43f824a8a6fd7493debc3edef1853
ethyashpathak <ethyashpathak@gmail.com>
2025-07-18 21:37:28 +0530

    debugging

--- blogsite/src/components/input.jsx
+++ blogsite/src/components/input.jsx
@@ -14,7 +14,7 @@ const Input=React.forwardRef(function Input({
              htmlFor={id}>
                 </label>
                 }
-                <Input type={type} 
+                <input type={type} 
                 className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full 
                     ${className}`}
                     ref={ref}
