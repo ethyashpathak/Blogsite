60e6001fc471d14f23216da64f44caaca4f8f00d
ethyashpathak <ethyashpathak@gmail.com>
2025-07-18 21:36:18 +0530

    debugging

--- blogsite/src/pages/Home.jsx
+++ blogsite/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import appwriteService from "../appwrite/config";
 import {Container, PostCard} from '../components'
+import { useSelector } from 'react-redux';
 
 function Home() {
     const [posts, setPosts] = useState([])
+    const authStatus=useSelector((state)=>state.auth.status)
 
     useEffect(() => {
         appwriteService.getPosts().then((posts) => {
@@ -20,7 +22,9 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
+                                {!authStatus && (
+                                     <h2>Login to Read Posts</h2>
+                                )} 
                             </h1>
                         </div>
                     </div>
